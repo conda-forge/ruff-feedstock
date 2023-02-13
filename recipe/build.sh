f1c17ed3e0b23c502a73b2edf8049c625737c663
Eli Rykoff <erykoff@stanford.edu>
2023-02-12 19:35:18 -0800

    Go to correct directory for bundling licenses.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,12 @@ set -ex
 # https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
+pushd ruff_cli
 # Bundle all downstream library licenses
 cargo-bundle-licenses \
   --format yaml \
   --output ${SRC_DIR}/THIRDPARTY.yml
+popd
 
 # Run the maturin build via pip which works for direct and
 # cross-compiled builds.
