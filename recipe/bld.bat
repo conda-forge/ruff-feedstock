085c1006550f80d76573e62374b50ba766a621f0
erykoff <erykoff@stanford.edu>
2023-02-13 05:59:53 -0800

    Try to fix cross-compilation and update to 0.0.245 (#94)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,17 +4,14 @@ REM Create temp folder
 mkdir tmpbuild_%PY_VER%
 set TEMP=%CD%\tmpbuild_%PY_VER%
 REM Bundle all downstream library licenses
+pushd crates\ruff_cli
 cargo-bundle-licenses ^
     --format yaml ^
     --output %SRC_DIR%\THIRDPARTY.yml ^
     || goto :error
-REM Use PEP517 to install the package
-maturin build --no-default-features --release -i %PYTHON%
-REM Install wheel
-cd target/wheels
-REM set UTF-8 mode by default
-chcp 65001
+popd
+REM Run the maturin build via pip
 set PYTHONUTF8=1
 set PYTHONIOENCODING="UTF-8"
 set TMPDIR=tmpbuild_%PY_VER%
-FOR %%w in (*.whl) DO %PYTHON% -m pip install %%w --no-clean
+%PYTHON% -m pip install . -vv
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,13 @@ set -ex
 # https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
+pushd crates/ruff_cli
 # Bundle all downstream library licenses
 cargo-bundle-licenses \
   --format yaml \
   --output ${SRC_DIR}/THIRDPARTY.yml
+popd
 
-# Apply PEP517 to install the package
-maturin build --no-default-features --release -i $PYTHON
-
-cd target/wheels
-
-# Install wheel manually
-$PYTHON -m pip install *.whl
+# Run the maturin build via pip which works for direct and
+# cross-compiled builds.
+$PYTHON -m pip install . -vv
